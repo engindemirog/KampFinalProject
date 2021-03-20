033a5c74d7460fab72cbe786e0c18377c2fb8e4c
Engin Demiroğ <engindemirog@gmail.com>
2021-03-20 23:03:44 +0300

    20 Mart Commit 1

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -34,7 +34,7 @@ namespace Business.Concrete
 
         //00.25 Dersteyiz
         //Claim
-        [SecuredOperation("product.add,admin")]
+        //[SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
         [CacheRemoveAspect("IProductService.Get")]
         public IResult Add(Product product)
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -77,6 +77,7 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.ConfigureCustomExceptionMiddleware();
 
             app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
